6fe0f64b833fe329331ca08c4323e24be1b3c6ad
Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>
2019-05-29 22:43:54 -0700

    Initialize a React project building with Webpack 4, Babel 7, react-hot-loader 4.8, and ESLint with Standard style

---
+++ src/components/Root.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { hot } from 'react-hot-loader/root'
+
+const Root = () => (
+  <p>Hello world!</p>
+)
+
+export default hot(Root)
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    port: process.env.PORT,
+    host: '0.0.0.0',
+    hot: true
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['react-hot-loader/babel']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
