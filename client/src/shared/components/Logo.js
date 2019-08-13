c62864c363440b155708b696f75f8fb924be9d72
Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>
2019-08-12 21:45:02 -0700

    Add blank alt text to cntct logo for HTML validity

--- client/src/shared/components/Logo.js
+++ client/src/shared/components/Logo.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import logo from '../resources/logo.svg'
 
-const Logo = () => <img src={logo} />
+const Logo = () => <img src={logo} alt='' /> // Alt is blank because the logo is a purely decorative element
 
 export default Logo
